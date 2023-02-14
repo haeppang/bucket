2dc88df444dc0823c374eb70c93a04244d611e5d
soyun-Kang <83630075+soyun-Kang@users.noreply.github.com>
2023-02-14 20:04:18 +0900

    Add files via upload

---
+++ 20230213/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.0bhvbkx.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+    buckets_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(buckets_list) + 1
+
+    doc = {
+        'num' : count,
+        'bucket' : bucket_receive,
+        'done' : 0
+    }
+    db.bucket.insert_one(doc)
+
+    return jsonify({'msg': '기록 완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+
+    return jsonify({'msg': '버킷 완료!'})
+
+@app.route("/bucket/cancel", methods=["POST"])
+def bucket_cencel():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
+
+    return jsonify({'msg': '버킷 취소!'})
+
+
+@app.route("/bucket/delete", methods=["POST"])
+def bucket_delete():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receive)})
+
+    return jsonify({'msg': '버킷 삭제!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
